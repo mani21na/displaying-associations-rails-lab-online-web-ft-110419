c250403b16a3a24dcc457bd8a4aa7cc81dfb401f
Hanna Lee <mani21na@gmail.com>
2020-02-07 18:11:53 +0000

    Automatically backed up by Learn

--- db/migrate/20200207000000_add_foreign_key_to_songs.rb
+++ db/migrate/20200207000000_add_foreign_key_to_songs.rb
@@ -1,6 +1,5 @@
-class AddForeignKeyToSongs < ActiveRecord::Migration 
-  def change 
-    add_column :songs, :artist_id, :integer 
+class AddForeignKeyToSongs < ActiveRecord::Migration
+  def change
+    add_column :songs, :artist_id, :integer
   end
 end
-  
